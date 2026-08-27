da8a5ec10791b640333ef4857963eb1ba2aa3a65
spiraletech <creeshchris@gmail.com>
2026-08-27 02:49:10 -0700

    Provide safe default provider capabilities

--- include/etherbeat/IModelBackend.hpp
+++ include/etherbeat/IModelBackend.hpp
@@ -13,7 +13,14 @@ public:
     virtual ~IModelBackend() = default;
 
     [[nodiscard]] virtual std::string_view name() const noexcept = 0;
-    [[nodiscard]] virtual ProviderCapabilities capabilities() const noexcept = 0;
+
+    // Existing text-to-music renderers remain source-compatible while newer
+    // providers can advertise more specialized capabilities explicitly.
+    [[nodiscard]] virtual ProviderCapabilities capabilities() const noexcept {
+        return capability(ProviderCapability::TextToInstrumental)
+            | ProviderCapability::DraftRole
+            | ProviderCapability::QualityRole;
+    }
 
     virtual GenerationArtifact generate(
         const GenerationRequest& request,
